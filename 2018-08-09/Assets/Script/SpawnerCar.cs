b9c44c3d62fae1badc64263ec755ec3fa58ec9b8
유홍석 <youhongseok442@gmail.com>
2018-08-21 15:44:41 +0900

    Car.Player.SpawnCar

---
+++ 2018-08-09/Assets/Script/SpawnerCar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SpawnerCar : MonoBehaviour {
+    public GameObject PrefebCar1;
+    private float nextTime = 0.0f;
+    public float spawnRate = 0.3f;
+    public BackController road;
+    private Player player;
+	// Use this for initialization
+	void Start () {
+        road = GameObject.Find("Road").GetComponent<BackController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		  if(nextTime<Time.time)
+        {
+            SpawnCar1();
+            nextTime = Time.time + spawnRate;
+        }
+	}
+   
+
+    void SpawnCar1()
+    {
+        float addXPos = Random.Range(-road.roadWidth, road.roadWidth);
+        Vector3 spawnPos = transform.position + new Vector3(addXPos, 0, 0);
+        Instantiate(PrefebCar1, spawnPos, Quaternion.identity);
+    }
+}
